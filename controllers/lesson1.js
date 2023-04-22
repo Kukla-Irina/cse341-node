e1e6bd9bd6e1817694613c8bae3f59dec5c51102
Kukla-Irina <kuk18002@byui.edu>
2023-04-22 16:23:27 +0300

    Update

---
+++ controllers/lesson1.js
@@ -0,0 +1,12 @@
+const marinaRoute = (req,res)=> {
+    res.send('Marina Kukla');
+};
+
+const arinaRoute = (req,res)=> {
+    res.send('Arina Kukla');
+};
+
+module.exports = {
+    marinaRoute,
+    arinaRoute
+};
